749beb78bf2c97f4b98d6f845760c039fa6cffd0
Viktor Jacynycz <viktor@jacynycz.es>
2017-05-08 14:18:13 +0200

    mejorado sistema de obtención de colores, mejorada eficiencia

--- spectrogram/spectrogram.py
+++ spectrogram/spectrogram.py
@@ -5,6 +5,7 @@ from scipy.io import wavfile
 import numpy as np
 from matplotlib.colors import hsv_to_rgb
 import argparse
+from heapq import *
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
@@ -13,18 +14,22 @@ ap.add_argument("-f", "--file", required=False, help="Path to the file")
 args = vars(ap.parse_args())
 
 # Grab your wav and filter it
-file = 'wavs/00/sample3.wav'
+file = 'wavs/00/samp2.wav'
 if (args["file"]):
     file = str(args["file"])
 filename = re.match( r'.*\/(.*?)\.wav', file, re.M|re.I).group(1)
 output = 'images/'+filename+'.png'
 print('Generating ',file,' chromoponia tag in ',output)
 rate, data2 = wavfile.read(file)
-data = []
+#data = []
 print('Merging stereo data...')
-for x in data2:
-    data.append(np.mean(x))
-
+#for x in data2:
+#    data.append(np.mean(x))
+print(len(data2))
+try:
+    data = (data2[:,0] + data2[:,1]) / 2
+except:
+    data = data2
 print('Stereo data merged...')
 
 from matplotlib import pyplot as plt
@@ -52,21 +57,28 @@ for steps in range(0,nsamples):
         n_steps = rangesample +  (bins.size % nsamples)
     else:
         n_steps = rangesample
-
+    h = []
     for step_columns in range(0,n_steps):
         index = step_columns + steps
 
         maxfreq = spectrogram_data[0][index]
 
         for step_frequency in range(0, freqs.size-1):
-            if step_frequency > 100 and step_frequency < 400:
-                if spectrogram_data[step_frequency][index] >= maxfreq:
-                    maxfreq = step_frequency
+            if step_frequency > 3 and step_frequency < 513:
+                #if spectrogram_data[step_frequency][index] >= maxfreq:
+                #    maxfreq = step_frequency
+                heappush(h, (spectrogram_data[step_frequency][index],step_frequency))
 
     aux_array = []
-    norm = maxfreq/(freqs.size-1)
-    deg = 360 * norm
-    col = hsv_to_rgb((deg,1,1))
+    largest = nlargest(20,h)
+    largestfreqs = []
+    for k,v in largest:
+        largestfreqs.append(pow(v,3))
+    maxfreq = np.mean(largestfreqs)
+    #norm = maxfreq/(freqs.size-1)
+    deg = (((pow(maxfreq,2)) % 350)+10.0)/360;
+    #print(deg)
+    col = hsv_to_rgb((deg,1.0,1.0))
 
     for h in range(0,nsamples):
         aux_array.append(col)
